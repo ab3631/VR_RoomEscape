d7d244297bfbf8ac130b97ca177e6595aeec7395
futureots <futureots113@gmail.com>
2025-05-02 00:01:31 +0900

    sortAlgorithm and add Dotween

--- Virtual Project/Assets/Scripts/GameObjectIndex.cs
+++ Virtual Project/Assets/Scripts/GameObjectIndex.cs
@@ -1,8 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameObjectIndex : MonoBehaviour
 {
-    public int index;
+    public TextMeshPro textMeshPro;
+    private int _index;
+    public int Index
+    {
+        get
+        {
+            return _index;
+        }
+        set
+        {
+            _index = value;
+            UpdateText();
+        }
+    }
+
+    private void Start()
+    {
+        textMeshPro = GetComponentInChildren<TextMeshPro>();
+    }
+    public void UpdateText()
+    {
+        textMeshPro.text = _index.ToString();
+    }
+
 }
--- Virtual Project/Assets/Scripts/SortAlgorithm.cs
+++ Virtual Project/Assets/Scripts/SortAlgorithm.cs
@@ -1,14 +1,16 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 
 using Unity.Mathematics;
-using Unity.VisualScripting;
 
 using UnityEngine;
 
 public class SortAlgorithm : MonoBehaviour
 {
-    public List<GameObjectIndex> boxList;
+    List<GameObjectIndex> boxList;
+    public int listNum;
+    public GameObject boxPrefab;
 
     private void Start()
     {
@@ -19,12 +21,12 @@ public class SortAlgorithm : MonoBehaviour
     public void SetBlock()
     {
         boxList = new List<GameObjectIndex>();
-        for(int i = 0; i < transform.childCount; i++)
+        for(int i = 0; i < listNum; i++)
         {
-            var childObj = transform.GetChild(i).gameObject;
-            var index = childObj.AddComponent<GameObjectIndex>();
+            var childObj = Instantiate(boxPrefab,transform);
+            var index = childObj.GetComponent<GameObjectIndex>();
             boxList.Add(index);
-            index.index = i + 1;
+            index.Index = i + 1;
         }
         // Shuffle
         for(int i = 0; i < boxList.Count; i++)
@@ -34,34 +36,39 @@ public class SortAlgorithm : MonoBehaviour
             boxList[i] = boxList[rand];
             boxList[rand] = t;
         }
+        for (int i = 0; i < boxList.Count; i++)
+        {
+            boxList[i].transform.localPosition = new Vector3(i * 1.2f, 0, 0);
+        }
     }
 
     Queue<int2> queue = new Queue<int2>();
     public void Sort()
     {
-
-        int2 i = queue.Dequeue();
-        QuickSort(i.x, i.y);
-        Debug.Log(queue.Count);
-
+        var temp = new Queue<int2>(queue);
+        queue.Clear();
+        while (temp.Count > 0) 
+        { 
+            int2 sortIndex = temp.Dequeue();
+            StartCoroutine(QuickSort(sortIndex.x, sortIndex.y));
+            Debug.Log(queue.Count);
+        }
     }
     
-    public void QuickSort(int start, int end)
+    public IEnumerator QuickSort(int start, int end)
     {
-        if (start >= end) return;
+        if (start >= end) yield break;
         int s = start;
         int e = end;
         var standard = boxList[e];
         e--;
         while (s <= e)
         {
-            bool a = IsBigger(boxList[s].index,standard.index);
-            bool b = IsBigger(standard.index, boxList[e].index);
+            bool a = IsBigger(boxList[s].Index,standard.Index);
+            bool b = IsBigger(standard.Index, boxList[e].Index);
             if (a && b)
             {
-                GameObjectIndex t = boxList[s];
-                boxList[s] = boxList[e];
-                boxList[e] = t;
+                yield return StartCoroutine(SwapObject(s, e));
             }
             else
             {
@@ -75,11 +82,31 @@ public class SortAlgorithm : MonoBehaviour
                 }
             }
         }
-        GameObjectIndex temp = boxList[s];
-        boxList[s] = boxList[end];
-        boxList[end] = temp;
-        queue.Enqueue(new int2(start, e));
-        queue.Enqueue(new int2(s + 1, end));
+        if (s != end) yield return StartCoroutine(SwapObject(s, end));
+        if (start < e)
+        {
+            queue.Enqueue(new int2(start, e));
+        }
+        if(s+1 < end)
+        {
+            queue.Enqueue(new int2(s + 1, end));
+        }
+        
+    }
+    public IEnumerator SwapObject(int a, int b)
+    {
+        
+        Vector3 posA = boxList[a].transform.localPosition;
+        Vector3 posB = boxList[b].transform.localPosition;
+        Sequence swapSequence = DOTween.Sequence();
+        DOTween.Sequence().Append(boxList[a].transform.DOLocalMove(posA + Vector3.up*2, 0.5f))
+            .Append(boxList[a].transform.DOLocalMove(posB, 0.5f));
+        DOTween.Sequence().Append(boxList[b].transform.DOLocalMove(posB + Vector3.up*2, 0.5f))
+            .Append(boxList[b].transform.DOLocalMove(posA, 0.5f));
+        yield return new WaitForSeconds(1.2f);
+        GameObjectIndex temp = boxList[a];
+        boxList[a] = boxList[b];
+        boxList[b] = temp;
     }
 
     public bool IsBigger(int a, int b)
