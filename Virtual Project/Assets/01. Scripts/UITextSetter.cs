f1df3e88482181096de1e1c01f879b541867d203
futureots <futureots113@gmail.com>
2025-06-10 17:32:38 +0900

    UI textSetter 클래스 생성

---
+++ Virtual Project/Assets/01. Scripts/UITextSetter.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UITextSetter : MonoBehaviour
+{
+    [Multiline]
+    public string text;
+
+    public void SetUIText()
+    {
+        UIManager.Instance.playerUI.SetDescription(text);
+    }
+}
